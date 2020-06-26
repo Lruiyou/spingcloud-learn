fbbd97870ebbd429d4c9fb5c9e6159c68246fe9b
Lruiyou <1607320438@qq.com>
2020-06-26 16:56:18 +0800

    引入discoveryClient进行服务发现

--- cloud-provider-payment8001/src/main/java/com/alan/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/alan/springcloud/controller/PaymentController.java
@@ -4,10 +4,14 @@ import com.alan.springcloud.common.CommonResult;
 import com.alan.springcloud.entity.Payment;
 import com.alan.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +22,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment")
     public CommonResult create(@RequestBody Payment payment){
@@ -39,5 +46,19 @@ public class PaymentController {
         }else {
             return new CommonResult(500,"没有对应记录，查询ID："+id,null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String service : services){
+            log.info("********service:"+service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance instance : instances){
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
