48bd162322d9388f9bade15dd045e4d7009d69f1
Lruiyou <1607320438@qq.com>
2020-06-27 18:35:19 +0800

    8004支付服务和80订单服务注册进zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/alan/springcloud/OrderZK80Application.java
@@ -0,0 +1,13 @@
+package com.alan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZK80Application {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZK80Application.class,args);
+    }
+}
