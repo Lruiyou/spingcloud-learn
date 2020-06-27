e0e01533229d07308a9e20b41d4eeef7fcc0d751
Lruiyou <1607320438@qq.com>
2020-06-27 23:31:47 +0800

    修改ribbon负载均衡算法为RandomRule

--- cloud-consumer-order80/src/main/java/com/alan/springcloud/Order80Application.java
+++ cloud-consumer-order80/src/main/java/com/alan/springcloud/Order80Application.java
@@ -1,11 +1,14 @@
 package com.alan.springcloud;
 
+import com.alan.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class Order80Application {
     public static void main(String[] args) {
         SpringApplication.run(Order80Application.class,args);
