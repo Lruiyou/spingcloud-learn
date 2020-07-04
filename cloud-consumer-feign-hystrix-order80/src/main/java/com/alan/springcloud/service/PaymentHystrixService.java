f64dd0c5073efd2f249debdf2b7b95921ceac7b1
Lruiyou <1607320438@qq.com>
2020-07-05 01:18:50 +0800

    使用HystrixCommend注解对服务进行降级

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/alan/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,14 @@
+package com.alan.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_Timeout(@PathVariable("id") Integer id);
+}
